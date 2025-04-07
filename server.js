be674e455b544574b0e7301d9c0a3f3fdeefdf97
shuntia <shunta@koga.us>
2025-04-07 16:50:50 -0700

    tweaks

--- server.js
+++ server.js
@@ -10,17 +10,6 @@ console.log("Starting server");
 
 wss.on("connection", (ws) => {
   console.log("Connection initiated");
-  if (waitingClient) {
-    console.log("Pairing clients.");
-    const peerA = waitingClient;
-    const peerB = ws;
-    peerA.peer = peerB;
-    peerB.peer = peerA;
-    waitingClient = null;
-  } else {
-    console.log("No waiting clients.");
-    waitingClient = ws;
-  }
 
   ws.on("message", (msg) => {
     try {
