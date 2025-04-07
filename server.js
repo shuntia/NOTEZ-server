0503f4bf75440866723af928ea9085eb9daa8965
shuntia <shunta@koga.us>
2025-04-07 14:11:29 -0700

    switch port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 // server.js
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 3000 });
+const wss = new WebSocket.Server({ port: process.env.PORT || 4000 });
 
 let waitingClient = null;
 
